25f0e5501571c0e7f2a94aa5ecf1fc6193366562
admin <k.kubo.private.mobile@gmail.com>
2020-04-24 04:38:08 +0900

    add files

---
+++ git-branching/01.git-og-decorate.sh
@@ -0,0 +1,7 @@
+# which commit origin/master points to
+# which commit local branches points to
+# which branch your head points to. Branch your head points to is your current branch.
+# next commit is created as children of your branch, and your branch moves forwad.
+git log --decorate
+git log --all
+git log --graph
---
+++ git-branching/02.git-checkout.sh
@@ -0,0 +1,4 @@
+# create new branch and checkout
+git branch new-branch
+git checkout new-branch
+# equals git checkout -b <new-branch-name>
